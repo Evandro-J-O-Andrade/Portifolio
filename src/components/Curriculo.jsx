7e89ca204bdea1b6219547e4028954f273cad4f2
Evandro Andrade <evandro_j.o.a@hotmail.com>
2025-08-19 03:52:56 -0300

    Update Curriculo.jsx

--- src/components/Curriculo.jsx
+++ src/components/Curriculo.jsx
@@ -4,28 +4,28 @@ function Curriculo() {
     {
       titulo: "Curso de React",
       descricao: "Curso avançado de React e desenvolvimento web.",
-      img: "src/assets/certificados/react.png"
+      img: "../assets/certificados/react.png"
     },
     {
       titulo: "Diploma de Desenvolvimento Web",
       descricao: "Diploma emitido pela instituição XYZ.",
-      img: "src/assets/certificados/web.webp"
+      img: "../assets/certificados/web.webp"
     },
     {
       titulo: "Curso de PHP e MySQL",
       descricao: "Aprendizado completo de back-end com PHP e MySQL.",
-      img: "src/assets/certificados/php.png"
+      img: "../assets/certificados/php.png"
     },
   ];
 
   const ferramentasCorporativas = [
     { nome: "TrueNAS", img: "https://avatars.githubusercontent.com/u/8131664?s=200&v=4" },
     { nome: "VMs", img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vagrant/vagrant-original.svg" },
-    { nome: "GLPI", img: "/src/assets/certificados/GLPI.png" },
+    { nome: "GLPI", img: "../assets/certificados/GLPI.png" },
     { nome: "Active Directory", img: "/src/assets/certificados/active-directory.png" },
-    { nome: "OPNsense", img: "/src/assets/projetos/opnsense.png" },
-    { nome: "PFsense", img: "/src/assets/projetos/pfsense.png"},
-    { nome: "Zabbix", img: "/src/assets/projetos/zabbix.jpg" },
+    { nome: "OPNsense", img: "../assets/projetos/opnsense.png" },
+    { nome: "PFsense", img: "../assets/projetos/pfsense.png"},
+    { nome: "Zabbix", img: "../assets/projetos/zabbix.jpg" },
     // Adicione mais ferramentas conforme necessário  
   ];
 
